ac1db6e0f37b0e7ca73cbed57b41220da6c6b405
povsister <pov@mahou-shoujo.moe>
2021-05-10 22:38:30 +0800

    fix signal handler

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -15,12 +15,14 @@ var (
 func SetupSignalHandler() <-chan struct{} {
 	globalSignalTermOnce.Do(func() {
 		globalSignalTerm = make(chan struct{})
-		c := make(chan os.Signal, 1)
+		c := make(chan os.Signal, 2)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		<-c
-		close(globalSignalTerm)
-		<-c
-		os.Exit(1)
+		go func() {
+			<-c
+			close(globalSignalTerm)
+			<-c
+			os.Exit(1)
+		}()
 	})
 	return globalSignalTerm
 }
